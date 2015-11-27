898ea32e404d5f0751609cb1e25eccf5bbb98350
Brian Ingles <github@emeraldwalk.com>
2015-11-27 10:17:16 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# vscode-onsave
-Visual Studio Code extension to run commands whenever a file is saved.
+# On Save for Visual Studio Code
+This extension allows configuring commands that get run whenever a file is saved in vscode.
+
+NOTE: Commands only get run when saving an existing file. Creating new files, and Save as... don't trigger the commands.
+
+## Features
+* Configure multiple commands that run when a file is saved
+* Regex pattern matching for files that trigger commands running
+* Sync and async support
+
+## Configuration
+Add "emeraldwalk.onsave" configuration to user or workspace settings.
+* "commands" - array of commands that will be run whenever a file is saved.
+  * "match" - a regex for matching which files to run commands on
+  * "cmd" - command to run. Can include parameters that will be replaced at runtime (see Placeholder Tokens section below).
+  * "isAsync" (optional) - defaults to false. If true, next command will be run before this one finishes.
+
+### Sample Config
+This sample configuration will run echo statements including the saved file path.
+In this sample, the first command is async, so the second command will get executed immediately even if first hasn't completed.
+Since the second isn't async, the third command won't execute until the second is complete.
+
+    "emeraldwalk.onsave": {
+		"commands": [
+			{
+				"match": ".*",
+				"isAsync": true,
+				"cmd": "echo 'I run for all files.'"
+			},
+			{
+				"match": "\\.txt$",
+				"cmd": "echo 'I am a .txt file ${file}.'"
+			},
+			{
+				"match": "\\.js$",
+				"cmd": "echo 'I am a .js file ${file}.'"
+			}
+		]
+	}
+
+## Commands
+The following commands are exposed in the command palette:
+* On Save: Enable
+* On Save: Disable
+
+## Placeholder Tokens
+Commands support placeholders similar to tasks.json.
+
+* ${workspaceRoot}: workspace root folder
+* ${file}: path of saved file
+* ${fileBasename}: saved file's basename
+* ${fileDirname}: directory name of saved file
+* ${fileExtname}: extension (including .) of saved file
+* ${cwd}: current working directory
---
+++ src/extension.ts
@@ -0,0 +1,119 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import {exec} from 'child_process';
+
+export function activate(context: vscode.ExtensionContext): void {
+
+	var extension = new OnSaveExtension(context);
+	extension.showStatusMessage();
+
+	vscode.commands.registerCommand('extension.emeraldwalk.enableOnSave', () => {
+		extension.isEnabled = true;
+	});
+
+	vscode.commands.registerCommand('extension.emeraldwalk.disableOnSave', () => {
+		extension.isEnabled = false;
+	});
+
+	vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+		extension.runCommands(document);
+	});
+}
+
+interface ICommand {
+	match?: string;
+	cmd: string;
+	isAsync: boolean;
+}
+
+interface IConfig {
+	commands: Array<ICommand>;
+}
+
+class OnSaveExtension {
+	private _outputChannel: vscode.OutputChannel;
+	private _context: vscode.ExtensionContext;
+	private _config: IConfig;
+
+	constructor(context: vscode.ExtensionContext) {
+		this._context = context;
+		this._config = <IConfig><any>vscode.workspace.getConfiguration('emeraldwalk.onsave');
+		this._outputChannel = vscode.window.createOutputChannel('On Save');
+	}
+
+	/** Recursive call to run commands. */
+	private _runCommands(commands: Array<ICommand>): void {
+		if (commands.length) {
+			var cfg = commands.shift();
+
+			this.showStatusMessage(`*** cmd start: ${cfg.cmd}`);
+
+			var child = exec(cfg.cmd);
+			child.stdout.on('data', data => this._outputChannel.append(data));
+			child.stderr.on('data', data => this._outputChannel.append(data));
+			child.on('exit', (e) => {
+				// if sync
+				if (!cfg.isAsync) {
+					this._runCommands(commands);
+				}
+			});
+
+			// if async, go ahead and run next command
+			if (cfg.isAsync) {
+				this._runCommands(commands);
+			}
+		}
+	}
+
+	public get isEnabled(): boolean {
+		return !!this._context.globalState.get('isEnabled', true);
+	}
+	public set isEnabled(value: boolean) {
+		this._context.globalState.update('isEnabled', value);
+		this.showStatusMessage();
+	}
+
+	public showStatusMessage(message?: string): void {
+		message = message || `On save ${this.isEnabled ? 'enabled': 'disabled'}.`;
+		this._outputChannel.appendLine(message);
+		vscode.window.setStatusBarMessage(message);
+	}
+
+	public runCommands(document: vscode.TextDocument): void {
+		if(!this.isEnabled) {
+			this.showStatusMessage();
+			return;
+		}
+
+		var commandConfigs = (this._config.commands || [])
+			.filter(cfg => new RegExp(cfg.match).test(document.fileName));
+
+		if (commandConfigs.length === 0) {
+			this._outputChannel.show();
+			this.showStatusMessage('No on save commands configured.');
+			return;
+		}
+
+		this.showStatusMessage('Running on save commands...');
+
+		// build our commands by replacing parameters with values
+		var commands: Array<ICommand> = [];
+		for (let cfg of commandConfigs) {
+			var cmdStr = cfg.cmd;
+
+			cmdStr = cmdStr.replace(/\${file}/g, `${document.fileName}`);
+			cmdStr = cmdStr.replace(/\${workspaceRoot}/g, `${vscode.workspace.rootPath}`);
+			cmdStr = cmdStr.replace(/\${fileBasename}/g, `${path.basename(document.fileName) }`);
+			cmdStr = cmdStr.replace(/\${fileDirname}/g, `${path.dirname(document.fileName) }`);
+			cmdStr = cmdStr.replace(/\${fileExtname}/g, `${path.extname(document.fileName) }`);
+			cmdStr = cmdStr.replace(/\${cwd}/g, `${process.cwd() }`);
+
+			commands.push({
+				cmd: cmdStr,
+				isAsync: !!cfg.isAsync
+			});
+		}
+
+		this._runCommands(commands);
+	}
+}
